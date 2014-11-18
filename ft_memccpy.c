f151d1065860f6dfbca8d67fd0ef35b9bf898e7a
alegent <alegent@student.42.fr>
2014-11-18 18:06:58 +0100

    correction strnew

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: alegent <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 19:01:47 by alegent           #+#    #+#             */
-/*   Updated: 2014/11/17 16:31:59 by alegent          ###   ########.fr       */
+/*   Updated: 2014/11/17 19:26:35 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void		*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		*(char *)dst = *(char *)src;
+		*(unsigned char *)dst = *(unsigned char *)src;
 		if (*(char *)src == (unsigned char)c)
 		{
 			dst++;
--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: alegent <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 11:19:08 by alegent           #+#    #+#             */
-/*   Updated: 2014/11/17 16:49:52 by alegent          ###   ########.fr       */
+/*   Updated: 2014/11/18 18:06:23 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ char		*ft_strnew(size_t size)
 {
 	char	*str;
 
-	str = (char *)malloc(sizeof(char) * size);
+	str = (char *)malloc(sizeof(char) * (size + 1));
 	if (str == NULL)
 		return (NULL);
+	str[size] = '\0';
 	return (str);
 }
