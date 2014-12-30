9bbd9112a5d7de71a5dd66ab48c67e51521eb39c
alegent <alegent@student.42.fr>
2014-12-30 12:20:25 +0100

    Modif inutil, ma lib a l'air de bien fonctionner ! Esperons que je la valide cette fois !

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: alegent <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 18:00:00 by alegent           #+#    #+#             */
-/*   Updated: 2014/11/07 18:40:09 by alegent          ###   ########.fr       */
+/*   Updated: 2014/12/30 12:18:49 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int			ft_strcmp(const char *s1, const char *s2)
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: alegent <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 18:12:20 by alegent           #+#    #+#             */
-/*   Updated: 2014/11/17 16:48:53 by alegent          ###   ########.fr       */
+/*   Updated: 2014/12/30 12:17:59 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int			ft_strncmp(const char *s1, const char *s2, size_t n)
 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
