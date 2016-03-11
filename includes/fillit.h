cadc8ede387aae1b157985529a8bad4a37ee1216
Alexandre LEGENT <alexandrelegent@gmail.com>
2016-03-11 15:12:55 +0100

    Verif du bon nombre d'argument, maintenant je vais tout de suite changer de branche et passer a la lecture, le stockage et la verification du fichier passer en parametre.

--- includes/fillit.h
+++ includes/fillit.h
@@ -1,5 +1,6 @@
 #ifndef FILLIT_H
 # define FILLIT_H
 # include "libft.h"
+# define PROG_NAME "fillit"
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,9 @@
 #include "fillit.h"
 
-int                 main(void)
+int                 main(int ac, char **av)
 {
+    if (ac != 2)
+        ft_puterror(PROG_NAME, "usage: ./fillit tetriminos_file");
+    ft_putendl(av[1]);
     return (42);
 }
