c99f8fe8ee3e6844678160eec8b03099ec0cb21f
Alexandre LEGENT <alexandrelegent@gmail.com>
2017-04-19 10:35:14 +0200

    Correct memory leaks on ft_putnbr_fd

--- print/ft_putnbr_fd.c
+++ print/ft_putnbr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alegent <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: alegent <alegent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 16:46:40 by alegent           #+#    #+#             */
-/*   Updated: 2014/11/05 11:52:36 by alegent          ###   ########.fr       */
+/*   Updated: 2017/04/19 10:34:21 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 void		ft_putnbr_fd(int n, int fd)
 {
+	char	*tmp;
+
+	tmp = ft_itoa(n);
 	ft_putstr_fd(ft_itoa(n), fd);
+	free(tmp);
 }
