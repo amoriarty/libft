31e97410e508cda99fadeac9b81173ea9c2da22a
Alex Legent <alexandrelegent@gmail.com>
2019-05-14 09:18:03 +0200

    Add Xcode projects to repository. Move all sources into Sources directory. Add a working Tests target, so we can write Swift tests.

---
+++ Tests/Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Tests.swift
+//  Tests
+//
+//  Created by Alex Legent on 14/05/2019.
+//  Copyright © 2019 Alex Legent. All rights reserved.
+//
+
+import XCTest
+
+class Tests: XCTestCase {
+
+    func testUnitTesting() {
+        ft_putendl("Hello World")
+    }
+
+}
