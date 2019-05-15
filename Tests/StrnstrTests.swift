9407bc0f16a6c31e572e183bd94dd5c6e62a95ba
Alex Legent <alexandrelegent@gmail.com>
2019-05-15 10:49:18 +0200

    ft_strnstr tests

---
+++ Tests/StrnstrTests.swift
@@ -0,0 +1,57 @@
+//
+//  StrnstrTests.swift
+//  Tests
+//
+//  Created by Alex Legent on 15/05/2019.
+//  Copyright © 2019 Alex Legent. All rights reserved.
+//
+
+import XCTest
+
+class StrnstrTests: XCTestCase {
+
+    func testBasic() {
+        let haystack = "ft_strnstr: basic test"
+        let needle = "ft_strnstr"
+
+        XCTAssert(ft_strnstr(haystack, needle, haystack.count) == strnstr(haystack, needle, haystack.count))
+    }
+
+    func testLargeSize() {
+        let haystack = String(repeating: "A", count: 0xFFFFFFF)
+        let needle = "ft_strnstr"
+        XCTAssert(ft_strnstr(haystack, needle, haystack.count) == strnstr(haystack, needle, haystack.count))
+    }
+
+    func testNoSize() {
+        let haystack = "ft_strnstr: no size test"
+        let needle = "ft_strnstr"
+
+        XCTAssert(ft_strnstr(haystack, needle, 0) == strnstr(haystack, needle, 0))
+    }
+
+    func testEmpty() {
+        let haystack = ""
+        let needle = "ft_strnstr"
+        XCTAssert(ft_strnstr(haystack, needle, haystack.count) == strnstr(haystack, needle, haystack.count))
+    }
+
+    func testNull() {
+        let haystack = "ft_strnstr: null test"
+        let needle = "ft_strnstr"
+        XCTAssert(ft_strnstr(nil, needle, haystack.count) == nil)
+        XCTAssert(ft_strnstr(haystack, nil, haystack.count) == nil)
+    }
+
+    func testPerfomance() {
+        let haystack = "ft_strnstr: perfomance test"
+        let needle = "ft_strnstr"
+
+        measure {
+            for _ in 0...1000 {
+                ft_strnstr(haystack, needle, haystack.count)
+            }
+        }
+    }
+
+}
