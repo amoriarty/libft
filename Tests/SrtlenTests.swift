ce9718b0fc100ba9a76eedc74c41a1ebee9ed8f2
Alex Legent <alexandrelegent@gmail.com>
2019-05-15 08:45:29 +0200

    ft_strlen tests

---
+++ Tests/SrtlenTests.swift
@@ -0,0 +1,37 @@
+//
+//  SrtlenTests.swift
+//  Tests
+//
+//  Created by Alex Legent on 15/05/2019.
+//  Copyright © 2019 Alex Legent. All rights reserved.
+//
+
+import XCTest
+
+class SrtlenTests: XCTestCase {
+
+    func testBasic() {
+        let source = "ft_strlen: basics test"
+        XCTAssert(ft_strlen(source) == source.count)
+    }
+
+    func testEmpty() {
+        let source = ""
+        XCTAssert(ft_strlen(source) == source.count)
+    }
+
+    func testNull() {
+        XCTAssert(ft_strlen(nil) == 0)
+    }
+
+    func testPerfomance() {
+        let source = "ft_strlen: perfomance test"
+
+        measure {
+            for _ in 0...1000 {
+                ft_strlen(source)
+            }
+        }
+    }
+
+}
