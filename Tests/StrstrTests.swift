0cacc839cf4344031c1fa32cd1837d813ca54772
Alex Legent <alexandrelegent@gmail.com>
2019-05-15 10:45:43 +0200

    ft_strstr tests

---
+++ Tests/StrstrTests.swift
@@ -0,0 +1,50 @@
+//
+//  StrstrTests.swift
+//  Tests
+//
+//  Created by Alex Legent on 15/05/2019.
+//  Copyright © 2019 Alex Legent. All rights reserved.
+//
+
+import XCTest
+
+class StrstrTests: XCTestCase {
+
+    func testBasic() {
+        let haystack = "ft_strstr: basic test"
+        let needle = "ft_strstr"
+
+        XCTAssert(ft_strstr(haystack, needle) == strstr(haystack, needle))
+    }
+
+    func testLargeSize() {
+        let haystack = String(repeating: "A", count: 0xFFFFFFF)
+        let needle = "ft_strstr"
+        XCTAssert(ft_strstr(haystack, needle) == strstr(haystack, needle))
+    }
+
+    func testEmpty() {
+        let haystack = ""
+        let needle = "ft_strstr"
+        XCTAssert(ft_strstr(haystack, needle) == strstr(haystack, needle))
+    }
+
+    func testNull() {
+        let haystack = "ft_strstr: null test"
+        let needle = "ft_strstr"
+        XCTAssert(ft_strstr(nil, needle) == nil)
+        XCTAssert(ft_strstr(haystack, nil) == nil)
+    }
+
+    func testPerfomance() {
+        let haystack = "ft_strstr: perfomance test"
+        let needle = "ft_strstr"
+
+        measure {
+            for _ in 0...1000 {
+                ft_strstr(haystack, needle)
+            }
+        }
+    }
+
+}
