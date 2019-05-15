21c77c70b1f48724a21a21286ba4a7e0143aa6de
Alex Legent <alexandrelegent@gmail.com>
2019-05-15 13:33:28 +0200

    ft_strsub tests

---
+++ Tests/StrsubTests.swift
@@ -0,0 +1,48 @@
+//
+//  StrsubTests.swift
+//  Tests
+//
+//  Created by Alex Legent on 15/05/2019.
+//  Copyright © 2019 Alex Legent. All rights reserved.
+//
+
+import XCTest
+
+class StrsubTests: XCTestCase {
+
+    func testBasic() {
+        let source = "ft_strsub: basic test"
+        let buffer = ft_strsub(source, 0, 9)
+
+        XCTAssert(buffer != nil)
+        XCTAssert(memcmp(source, buffer!, 9) == 0)
+
+        free(buffer)
+    }
+
+    func testNoSize() {
+        let source = "ft_strsub: no size test"
+        let buffer = ft_strsub(source, 0, 0)
+
+        XCTAssert(buffer != nil)
+        XCTAssert(memcmp(buffer!, "", 2) == 0)
+
+        free(buffer)
+    }
+
+    func testNull() {
+        XCTAssert(ft_strsub(nil, 0, 42) == nil)
+    }
+
+    func testPerfomance() {
+        let source = "ft_strsub: basic test"
+
+        measure {
+            for _ in 0...1000 {
+                let buffer = ft_strsub(source, 0, 9)
+                free(buffer)
+            }
+        }
+    }
+
+}
