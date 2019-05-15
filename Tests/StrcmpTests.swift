186a72af3e56cd856bc5bbb8af6f16361306dfac
Alex Legent <alexandrelegent@gmail.com>
2019-05-15 10:52:37 +0200

    ft_strcmp tests

---
+++ Tests/StrcmpTests.swift
@@ -0,0 +1,48 @@
+//
+//  StrcmpTests.swift
+//  Tests
+//
+//  Created by Alex Legent on 15/05/2019.
+//  Copyright © 2019 Alex Legent. All rights reserved.
+//
+
+import XCTest
+
+class StrcmpTests: XCTestCase {
+
+    func testBasic() {
+        let left = "ft_strcmp: basics test"
+        let right = "ft_strcmp: basics tests"
+
+        XCTAssert(ft_strcmp(left, left) == strcmp(left, left))
+        XCTAssert(ft_strcmp(left, right) == strcmp(left, right))
+        XCTAssert(ft_strcmp(left, right) == strcmp(left, right))
+    }
+
+    func testLargeSize() {
+        let source = String(repeating: "A", count: 0xFFFFFF)
+        XCTAssert(ft_strcmp(source, source) == 0)
+    }
+
+    func testEmpty() {
+        XCTAssert(ft_strcmp("", "") == 0)
+    }
+
+    func testNull() {
+        let source = "ft_strcmp: null test"
+        XCTAssert(ft_strcmp(nil, source) == 0)
+        XCTAssert(ft_strcmp(source, nil) == 0)
+        XCTAssert(ft_strcmp(nil, nil) == 0)
+    }
+
+    func testPerformance() {
+        let source = "ft_strcmp: performance test"
+
+        measure {
+            for _ in 0...1000 {
+                ft_strcmp(source, source)
+            }
+        }
+    }
+
+}
